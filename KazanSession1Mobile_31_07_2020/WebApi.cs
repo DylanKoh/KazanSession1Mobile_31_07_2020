e904e1a96ddbeb59dd517adb16d6736fd0af1547
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-07-31 15:28:27 +0800

    Add project files.

---
+++ KazanSession1Mobile_31_07_2020/WebApi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KazanSession1Mobile_31_07_2020
+{
+    public class WebApi
+    {
+        private HttpClient client = new HttpClient();
+        string baseAddress = "http://10.0.2.2:49321/";
+        public async Task<string> PostAsync(string urlName, string Json)
+        {
+            var newUrl = baseAddress + urlName;
+            if (Json == null)
+            {
+                var stringContent = new StringContent("", Encoding.UTF8, "application/json");
+                var responseString = await client.PostAsync(newUrl, stringContent).Result.Content.ReadAsStringAsync();
+                return responseString;
+            }
+            else
+            {
+                var stringContent = new StringContent(Json, Encoding.UTF8, "application/json");
+                var responseString = await client.PostAsync(newUrl, stringContent).Result.Content.ReadAsStringAsync();
+                return responseString;
+            }
+        }
+    }
+}
